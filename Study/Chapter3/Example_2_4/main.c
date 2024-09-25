a22a66b2b4105c971cac83524d890c0545833905
Kinesis <lordkinesis@naver.com>
2024-09-25 14:41:10 +0900

    :hammer: Update: main.c in Example_2_4

---
+++ Study/Chapter3/Example_2_4/main.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+	char ch = 'A';
+
+	printf("%c¿« ASCII Code = %d\n", ch, ch);
+	printf("%c¿« ASCII Code = %d\n", ch + 1, ch + 1);
+	printf("%c¿« ASCII Code = %d\n", ch + 2, ch + 2);
+
+	return 0;
+}
