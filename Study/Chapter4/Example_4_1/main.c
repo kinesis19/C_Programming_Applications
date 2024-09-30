f72871f9bf68710cbc3b87f106b29e170bb58aa0
Kinesis <lordkinesis@naver.com>
2024-09-30 13:54:54 +0900

    :hammer: Update: main.c in Example_4_1

---
+++ Study/Chapter4/Example_4_1/main.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	int num1, num2;
+	
+	printf("두 양의 정수를 입력하세요 : ");
+	scanf("%d %d", &num1, &num2);
+
+	printf("%d를 %d로 나눈 몫은 %d, 나머지는 %d입니다.\n", num1, num2, num1 / num2, num1 % num2);
+
+	return 0;
+}
