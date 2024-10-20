4087d029203ccd7f0b9261f5e8825e3d9ed69c7f
kinesis19 <lordkinesis@naver.com>
2024-10-21 08:21:37 +0900

    :hammer: Create: Task6_10.c

---
+++ Practicing/Chapter6/Task6_10.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+    int i, j;
+
+    for(i = 0; i < 5; i++){
+        for(j = 0; j <= i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
