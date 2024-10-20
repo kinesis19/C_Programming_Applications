3e853ab62e88c8e5e50040375a0383b552307852
kinesis19 <lordkinesis@naver.com>
2024-10-20 15:07:58 +0900

    :hammer: Create: Task2_1.c

---
+++ Practicing/Chapter3/Task2_1.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+    int n1, n2;
+
+    printf("정수형 int의 크기는 %zu입니다.\n", sizeof(int));
+    printf("정수형 long의 크기는 %zu입니다.\n", sizeof(long));
+    printf("정수형 long long의 크기는 %zu입니다.\n", sizeof(long long));
+
+    printf("정수를 입력하시오 (10진수): ");
+    scanf("%d", &n1);
+    printf("%d는 10진수로 %d, 8진수로 %#o, 16진수로 %#x입니다.\n", n1, n1, n1, n1);
+
+
+    printf("정수를 입력하시오 (16진수): ");
+    scanf("%x", &n2);
+    printf("%d는 10진수로 %d, 8진수로 %#o, 16진수로 %#x`입니다.\n", n2, n2, n2, n2);
+
+	return 0;
+}
