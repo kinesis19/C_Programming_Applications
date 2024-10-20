10e2c093dfbd4bc3ffb8d22c41b23cf5df4357b5
kinesis19 <lordkinesis@naver.com>
2024-10-20 14:39:26 +0900

    :hammer: Create: Task1_3.c

---
+++ Practicing/Chapter2/Task1_3.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+
+	printf("%c는 문자입니다.\n", 'A');
+	printf("%d는 정수입니다.\n", 123);
+	printf("%f는 실수입니다.\n", 3.141592);
+
+	return 0;
+}
