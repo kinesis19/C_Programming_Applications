78efc571b852efad205f719a1401fdbf6d657cab
kinesis19 <lordkinesis@naver.com>
2024-10-20 16:14:42 +0900

    :hammer: Create: Task2_6.c

---
+++ Practicing/Chapter3/Task2_6.c
@@ -0,0 +1,24 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    
+    char name;
+    int schoolId, sub1, sub2, sub3, sub4;
+    double avg = 0;
+
+    printf("이름: ");
+    scanf("%c", &name);
+
+    printf("학번: ");
+    scanf("%d", &schoolId);
+
+    printf("성적: ");
+    scanf("%d %d %d %d", &sub1, &sub2, &sub3, &sub4);
+
+    avg = (sub1 + sub2 + sub3 + sub4) / 4.0;
+    
+    printf("학생 %c(학번: %d)의 평균 성적은 %.2lf입니다.", name, schoolId, avg);
+
+    return 0;
+}
