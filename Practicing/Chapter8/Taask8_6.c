4614375e3cdada1588bcf3a8012c4a173136fb76
kinesis19 <lordkinesis@naver.com>
2024-12-08 02:24:11 +0900

    :hammer: Create: Task8_6

---
+++ Practicing/Chapter8/Taask8_6.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int compute_sum(int n);
+
+int main() {
+
+    int sum;
+    sum = compute_sum(100);
+    printf("1부터 100까지의 합 = %d\n", sum);
+
+    return 0;
+}
+
+int compute_sum(int n) {
+    int i;
+    int result = 0;
+
+    for (i = 0; i <= n; i++) {
+        result += i;
+    }
+
+    return result;
+}
