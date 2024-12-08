56e2370d92f0818cfc622737f74edcd182f1c755
kinesis19 <lordkinesis@naver.com>
2024-12-09 01:33:51 +0900

    :hammer: Update: Task8_17

--- Practicing/Chapter8/Task8_17.c
+++ Practicing/Chapter8/Task8_17.c
@@ -8,7 +8,7 @@ int main () {
     srand((unsigned)time(NULL));
 
     for (int i = 0; i < 6; i++) {
-        r = rand() % 46;
+        r = rand() % 45 + 1; // 로또는 숫자 1부터 ~ 45까지 존재함. 따라서 rand() % 45 + 1로 로직을 변경해 줘야 함.
         printf("%d, ", r);
     }
 
