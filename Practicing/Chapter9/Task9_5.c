77576cdb9086997fe1cb06bda5747d9d3c729a4e
kinesis19 <lordkinesis@naver.com>
2024-12-09 01:57:26 +0900

    :hammer: Create: Task9_5

--- Practicing/Chapter9/Task9_5.c
+++ Practicing/Chapter9/Task9_5.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
-void modify(int value) {
-    value = 99;
+void modify(int *ptr) {
+
+    *ptr = 99;
+
 }
 
 int main () {
 
     int number = 1;
-    
-    modify(number);
+    modify(&number);
+
     printf("number = %d\n", number);
 
     return 0;
