ba26be964e6ba662b270700c1df541e59bd4ab7a
kinesis19 <lordkinesis@naver.com>
2024-12-09 01:43:14 +0900

    :hammer: Create: Task9_3

---
+++ Practicing/Chapter9/Task9_3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+
+    int *pc, *pi, *pd;
+
+    printf("pc = %p, pc + 1 = %p, pc + 2 = %p\n\n", pc, pc + 1, pc + 2);
+    printf("pi = %p, pi + 1 = %p, pi + 2 = %p\n\n", pi, pi + 1, pi + 2);
+    printf("pd = %p, pd + 1 = %p, pd + 2 = %p\n\n", pd, pd + 1, pd + 2);
+
+
+    return 0;
+}
