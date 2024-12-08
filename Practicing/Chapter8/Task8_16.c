4a8687e9e995b0373c7154cc485c1c5946e33dcb
kinesis19 <lordkinesis@naver.com>
2024-12-09 01:23:36 +0900

    :hammer: Create: Task8_16

---
+++ Practicing/Chapter8/Task8_16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+
+int main () {
+
+    system("cls");
+
+    Sleep(2000);
+    printf("**********");
+    system("cls");
+    system("ipconfig");
+
+}
